076f7c56019e0048702581b14eda2ca038db1000
SirRipster <157152655+SirRipster@users.noreply.github.com>
2025-03-14 21:23:23 +0000

    Deploying to gh-pages from @ SirRipster/SirRipster.github.io@6d4ef513ea5e78642c31c85a7a3f39b8ccc7e406 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,7 +45,32 @@ ninja.data = [{
           window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
-    },{
+    },{id: "projects-fintech-competition",
+          title: 'FinTech Competition',
+          description: "Competed agasist 7 other for a 2000 dollar scholoarship",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
